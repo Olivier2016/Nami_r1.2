7c83936d4093af46d20f46669b833c812208b44f
Olivier2016 <olivier.robin@axa-tech.com>
2016-09-03 19:03:51 +0200

    Update footer.php

--- wp-content/themes/twentyfourteen/footer.php
+++ wp-content/themes/twentyfourteen/footer.php
@@ -19,7 +19,7 @@
 			<div class="site-info">
 				<?php do_action( 'twentyfourteen_credits' ); ?>
 				
-				<a href="<?php echo esc_url( __( 'https://www.axa.fr/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>			
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>			
 <!--				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyfourteen' ), 'WordPress' ); ?></a>
 -->			
 			</div><!-- .site-info -->
